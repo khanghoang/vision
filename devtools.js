42ee2325c253be581ca0766fce6545c25087e00c
Khang Hoang <hoangtrieukhang@gmail.com>
2016-04-15 19:37:09 +0700

    demo index.html

--- devtools.js
+++ devtools.js
@@ -3,4 +3,4 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Forecast", "toast.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("Forecast", "toast.png", "app/index.html", function(panel) {});
