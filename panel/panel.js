da4d1b1891bc16acfd0baaab45d3c0c5b80b194c
Khang Hoang <hoangtrieukhang@gmail.com>
2016-04-17 12:08:53 +0700

    add comment

--- panel/panel.js
+++ panel/panel.js
@@ -1,3 +1,9 @@
+// This one acts in the context of the panel in the Dev Tools
+//
+// Can use
+// chrome.devtools.*
+// chrome.extension.*
+
 var backgroundPageConnection = chrome.runtime.connect({
   name: 'panel'
 });
